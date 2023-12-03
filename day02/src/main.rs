d2213fc56bd7224a8de2c37eaf9bdb073c81de30
RickdeJager <rickdejager99@gmail.com>
2023-12-03 14:51:58 +0100

    add bench numbers for day02

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,8 +3,8 @@ use day02::{part1, part2, read_input};
 fn main() {
     let input = read_input("input.txt");
     let res = part1(&input);
-    println!("Part 1: {}", res); // 1867
+    println!("Part 1: {}", res); // 1867 (180.02 ns)
 
     let res = part2(&input);
-    println!("Part 2: {}", res); // 84538
+    println!("Part 2: {}", res); // 84538 (265.32 ns)
 }
