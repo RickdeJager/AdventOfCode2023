d2d1c87fa65a1cf2d623e1c71c032a0549663a82
RickdeJager <rickdejager99@gmail.com>
2023-12-08 12:08:50 +0100

    Refactor to remove hashsets and string allocations

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,20 +1,24 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::collections::HashMap;
 
-type Label = u32
+type Label = u16;
 
-#[derive(Debug)]
+#[derive(Debug, Default, Clone, Copy)]
 pub struct Node {
-    left: String,
-    right: String,
+    left: Label,
+    right: Label,
 }
 
 #[derive(Debug)]
 pub struct PuzzleInput {
     // left = true, right = false
     directions: Vec<bool>,
-    nodes: HashMap<String, Node>
+    // A huge sparse array (65k*sizeof(Node)) as a standin for a hashmap.
+    // as long as you're willing to accept that we can fit each label in a u16
+    // this gives a huge performance boost
+    map: Vec<Node>,
+    // Because we don't use a hashmap anymore, we need to track which keys are valid
+    nodes: Vec<Label>,
 }
 
 // https://rosettacode.org/wiki/Least_common_multiple#Rust
@@ -38,39 +42,66 @@ fn lcm(a: usize, b: &usize) -> usize {
     a * b / gcd(a, *b)
 }
 
+#[inline(always)]
+fn key_from_str(s: &str) -> Label {
+    s.bytes()
+        .enumerate()
+        .take(3)
+        .map(|(i, b)| ((b - b'A') as Label) << (i * 5))
+        .sum()
+}
+
+#[inline(always)]
+fn is_start_node(l: Label) -> bool {
+    (l >> (2 * 5) & 0b11111) == 0u16
+}
+
+#[inline(always)]
+fn is_end_node(l: Label) -> bool {
+    (l >> (2 * 5) & 0b11111) == (b'Z' - b'A') as u16
+}
 
 pub fn part1(input: &PuzzleInput) -> usize {
-    let mut cur_pos = "AAA";
+    let mut cur_pos = key_from_str("AAA");
+    let end = key_from_str("ZZZ");
     let mut i = 0;
-    while cur_pos != "ZZZ" {
-        let options = input.nodes.get(cur_pos).unwrap();
+    while cur_pos != end {
+        let options = input.map[cur_pos as usize];
         cur_pos = match input.directions[i % input.directions.len()] {
-            true => options.left.as_str(),
-            false => options.right.as_str(),
+            true => options.left,
+            false => options.right,
         };
         i += 1;
     }
-    
+
     i
 }
 
 pub fn part2(input: &PuzzleInput) -> usize {
-    let mut start_nodes: Vec<&str> = input.nodes.keys().filter(|node| node.ends_with('A')).map(|node| node.as_str()).collect();
-    let cycles: Vec<usize> = start_nodes.iter_mut().map(|node| {
-        let mut i = 0;
-        while node.as_bytes()[2] != b'Z' {
-            let options = input.nodes.get(&node.to_string()).unwrap();
-            *node = match input.directions[i % input.directions.len()] {
-                true => options.left.as_str(),
-                false => options.right.as_str(),
-            };
-            i += 1;
-        }
-        i
-    }).collect();
-    
-    let res = cycles.iter().fold(1usize, lcm);
-    
+    let mut start_nodes: Vec<Label> = input
+        .nodes
+        .iter()
+        .copied()
+        .filter(|x| is_start_node(*x))
+        .collect();
+    let cycles: Vec<usize> = start_nodes
+        .iter_mut()
+        .map(|node| {
+            let mut i = 0;
+            while !is_end_node(*node) {
+                let options = input.map[*node as usize];
+                *node = match input.directions[i % input.directions.len()] {
+                    true => options.left,
+                    false => options.right,
+                };
+                i += 1;
+            }
+            i
+        })
+        .collect();
+
+    let res = cycles.iter().fold(1, lcm);
+
     res
 }
 
@@ -80,12 +111,21 @@ pub fn read_input(filename: &str) -> PuzzleInput {
     let mut iter = reader.lines().flatten();
     let directions = iter.next().unwrap().chars().map(|c| c == 'L').collect();
     iter.next();
-    let nodes = iter.map(|line| {
-        let (key, tuple) = line.split_once(" = ").unwrap();
-        let (left, right) = tuple[1..tuple.len()-1].split_once(", ").unwrap();
-        let (left, right) = (left.to_string(), right.to_string());
-        (key.to_string(), Node{left, right})
-    }).collect();
+    let mut map = vec![Node::default(); 65536];
+    let nodes = iter
+        .map(|line| {
+            let (key, tuple) = line.split_once(" = ").unwrap();
+            let (left, right) = tuple[1..tuple.len() - 1].split_once(", ").unwrap();
+            let (left, right) = (key_from_str(left), key_from_str(right));
+            let key = key_from_str(key);
+            map[key as usize] = Node { left, right };
+            key
+        })
+        .collect();
 
-    PuzzleInput { directions, nodes }
+    PuzzleInput {
+        directions,
+        nodes,
+        map,
+    }
 }
--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,10 +1,10 @@
 use day08::{part1, part2, read_input};
 
 fn main() {
-    let input = read_input("input.txt"); // ()
+    let input = read_input("input.txt"); // (46.478 µs)
     let res = part1(&input);
-    println!("Part 1: {}", res); // 19951 ()
+    println!("Part 1: {}", res); // 19951 (32.710 µs)
 
     let res = part2(&input);
-    println!("Part 2: {}", res); // 16342438708751 ()
+    println!("Part 2: {}", res); // 16342438708751 (149.42 µs)
 }
