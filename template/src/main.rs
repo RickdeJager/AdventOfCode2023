5590c33ed673e23bf905775c1cd43c0c4b8f164c
RickdeJager <rickdejager99@gmail.com>
2023-12-24 12:48:46 +0100

    update template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,7 +1,12 @@
 use dayXX::{part1, part2, read_input};
 
 fn main() {
-    let input = read_input("input.txt");
+    // If there is an argument, use it as the input file name. Otherwise, use "input.txt"
+    let input_file_name = std::env::args()
+        .nth(1)
+        .unwrap_or("input.txt".to_string());
+
+    let input = read_input(&input_file_name);
     let res = part1(&input);
     println!("Part 1: {}", res);
 
